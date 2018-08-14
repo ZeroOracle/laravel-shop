66947367f295b6394093f31c66b7ec5d80ef0099
ZeroOracle <1450820946@qq.com>
2018-08-14 14:21:08 +0000

    微信支付

--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,29 @@ Route::group(['middleware' => 'auth'], function() {
 
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
         Route::group(['middleware' => 'email_verified'], function() {
-                Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
-                Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
-                Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
-                Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
-                Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
-                Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-                Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
-                Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
-                Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
-                Route::post('cart', 'CartController@add')->name('cart.add');
-                Route::get('cart', 'CartController@index')->name('cart.index');
-                Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
-                Route::post('orders', 'OrdersController@store')->name('orders.store');
-                Route::get('orders', 'OrdersController@index')->name('orders.index');
-                Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
-                Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
-
+            Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+            Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+            Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+            Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+            Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+            Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+            Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+            Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+            Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+            Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+            Route::post('cart', 'CartController@add')->name('cart.add');
+            Route::get('cart', 'CartController@index')->name('cart.index');
+            Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+            Route::post('orders', 'OrdersController@store')->name('orders.store');
+            Route::get('orders', 'OrdersController@index')->name('orders.index');
+            Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+            Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
 
     });
 
 });
+
+Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
