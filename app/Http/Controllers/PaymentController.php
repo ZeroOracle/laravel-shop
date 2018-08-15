48fa4b7f7f0f4c44cc78f6ce23d16a6b5cf4bcf3
ZeroOracle <1450820946@qq.com>
2018-08-15 04:57:34 +0000

    完善支付后逻辑

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -64,6 +64,9 @@ class PaymentController extends Controller
             'payment_no'     => $data->trade_no, // 支付宝订单号
         ]);
 
+
+        $this->afterPaid($order);
+
         return app('alipay')->success();
     }
 
@@ -113,6 +116,12 @@ class PaymentController extends Controller
             'payment_no'     => $data->transaction_id,
         ]);
 
+        $this->afterPaid($order);
         return app('wechat_pay')->success();
+    }
+
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 }
