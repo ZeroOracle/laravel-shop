0a41c80f6a26b84c3678181f7e4b37563eb5aa27
ZeroOracle <1450820946@qq.com>
2018-08-15 08:48:47 +0000

    管理后台
    订单列表

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::group(['middleware' => 'auth'], function() {
             Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
             Route::post('orders', 'OrdersController@store')->name('orders.store');
             Route::get('orders', 'OrdersController@index')->name('orders.index');
+            Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
             Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
             Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
 
